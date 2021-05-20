0d7cad53cdc8a089f24fc02204fbc20ba9211e5d
Vicente <vice.carmona@duocuc.cl>
2021-05-19 21:06:33 -0400

    spotify api

--- superApi.js
+++ superApi.js
@@ -26,6 +26,33 @@ $(document).ready(function(){
         })
     })
 
+    $("#traer-musica").click(function(){
+        var id = '6ec2joVXjW8GCD7MfIf06y'
+        $.get({
+            url: `https://api.spotify.com/v1/artists/${id}/albums`,
+            headers: {Authorization: "Bearer " + "BQBWb3sS_xn21uYuSgtpLsu4WbUzKXadYWfabMj6uaUTAOfn5q7Bu3gkNpxJ7NzuZBKXOCLb5Bgkz-YoW2jjdQy0Div2uSZF3pWI7fZ2ghcWTDW50MnB1z-Q-k7zVXdxTN9quBPOm1VAXFFV"},
+            success: function(response){
+                console.log(response)
+                var tabla = $("#cat-comida tbody")
+                tabla.empty();
+                $.each(response.items, function(index, album){
+                    tabla.append(`<tr> <th scope="row"> ${album.id} </th> + 
+                    <td scope="row"> ${album.name}</td> +
+                    <td scope="row"> <img src="${album.images[1].url}" alt=""></td> +
+                    <td scope="row">${album.release_date} </td>
+
+                    </tr>`)
+
+
+                    
+                })
+            },
+            error: function(error){
+                console.error(error);
+            }
+        })
+    })
+
 
 
 
